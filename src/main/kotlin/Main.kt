9f96d23ea7446c31131f2867a10885e0cdf6b866
misostack <gmmiso88@gmail.com>
2025-09-09 10:37:08 +0700

    idioms

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -175,9 +175,124 @@ fun sayHello(){
     }
 }
 
+data class Customer(
+    val id: Long,
+    val name: String,
+    val email: String,
+    val phone: String,
+){
+    var address: String? = null
+}
+
+fun idioms() {
+    // dtos: data transfer objects
+    // pojos: plain old java objects
+    // pocos: plain old old classes
+
+    val customer = Customer(
+        id=1,
+        name="miso",
+        email="misostack@gmail.com",
+        phone="0123456789",
+    )
+    customer.address = "123 Main St, City, Country"
+    println(customer)
+    println(customer.address)
+
+    val cloneCustomer = customer.copy(id=2,name="john doe", email = "kotlin@example.org")
+    println(cloneCustomer)
+    cloneCustomer.address = "321 Main St, City, Country"
+    println(cloneCustomer.address)
+    println(customer.address)
+
+    // comparing two objects
+    data class Point(val x: Int, val y: Int)
+    val point1 = Point(1,2)
+    val point2 = Point(y=2, x=1)
+    println(point1.equals(point2))
+    println(point1.hashCode() == point2.hashCode())
+    println(point1.toString() == point2.toString())
+    // destructuring
+    val (id, name, email) = customer
+    println("id: $id, name: $name, email: $email")
+
+    fun getPoint(): Point {
+        return Point(10, 20)
+    }
+    val (x, y) = getPoint()
+    println("x: $x, y: $y")
+    // collection of data
+    val months = Array<Int>(12) { 0}
+    for (i in months.indices) {
+        months[i] = i + 1
+    }
+    months.forEach { month -> println(month) }
+    val monthNames = arrayOf(
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    )
+    monthNames.forEachIndexed { index, month -> println("${index + 1}: $month") }
+
+    val numbers = MutableList<Int>(0){0}
+    println(numbers.size)
+    numbers.add(0)
+    println(numbers.size)
+
+    val daysInMonths = mapOf(
+        "January" to 31,
+        "February" to 28,
+        "March" to 31,
+        "April" to 30,
+        "May" to 31,
+        "June" to 30,
+        "July" to 31,
+        "August" to 31,
+        "September" to 30,
+        "October" to 31,
+        "November" to 30,
+        "December" to 31
+    )
+    for ((month, days) in daysInMonths) {
+        println("$month has $days days")
+    }
+
+    val votes : MutableMap<String, Int> = HashMap()
+    votes["Alice"] = 0
+    votes["Bob"] = 0
+    votes["Charlie"] = 0
+
+    votes["Alice"] = votes["Alice"]?.plus(1) ?: 0
+    votes["Bob"] = 30
+    votes["Charlie"] = votes["Charlie"]?.plus(1) ?: 0
+
+    println(votes)
+    for((name, vote) in votes) {
+        println("$name has $vote votes")
+    }
+
+    val gifts:  MutableMap<String, String> = LinkedHashMap()
+    gifts["Alice"] = "Book"
+    gifts["Bob"] = "Game"
+    gifts["Charlie"] = "Pen"
+    println(gifts)
+    for ((name, _) in gifts) {
+        println(name)
+    }
+
+    // filters, maps, reduces
+    val randomNumbers = List(100) { Random().nextInt(1000) }
+    val evenNumbers = randomNumbers.filter { it % 2 == 0 }
+    println("Even numbers: $evenNumbers")
+    val squaredNumbers = randomNumbers.map { it * it }
+    println("Squared numbers: $squaredNumbers")
+    val sum = randomNumbers.reduce { acc, i -> acc + i }
+    println("Sum of numbers: $sum")
+}
+
 fun main(args: Array<String>) {
     // jsonDemo()
     // helloWorld(args)
     // balanceMananagement()
-    sayHello()
+    // sayHello()
+    idioms()
 }
